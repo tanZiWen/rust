1ef48c46d35d44f17a14873bf3d6dac7e80e1eb2
tanyuan <1067598718@qq.com>
2020-07-01 15:12:14 +0800

    add loops

---
+++ loops/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+    println!("lift off!!!");
+
+    let a = [10, 20, 30 , 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+
+}
